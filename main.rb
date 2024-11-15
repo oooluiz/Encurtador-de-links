b6579cdd2794c2fdd7a03fd47aa5b87d7287e08c
ooluiz <luizfelip079@gmail.com>
2024-11-15 15:34:51 -0300

    

--- main.rb
+++ main.rb
@@ -3,19 +3,27 @@ require 'haml'
 #require 'extralite'
 require_relative 'db'
 
-set :haml, format: :html5
 
-get '/' do 
-  haml :index, :locals=>{:shortened_url => ""}
+class App < Sinatra::Base
+  set :haml, format: :html5
+
+  @@shortened_url = "dhdjjd"
+
+  get '/' do 
+#    puts @@shortened_url
+    haml :index, :locals=>{:shortened_url => @@shortened_url}
+  end
+
+  post '/' do
+    @@shortened_url = Db::short params["url"].gsub("http://","").gsub("https://","")
+    redirect "/"
+  end
+
+  get '/:url' do
+    #haml "%h1 #{params[:url]}"
+    url = Db.unshort params[:url]
+    redirect "https://#{url}"
+  end
 end
 
-post '/' do
-  shortened_url = Db::short params["url"].gsub("http://","").gsub("https://","")
-  haml :index, :locals => {:shortened_url => "/#{shortened_url}"}
-end
-
-get '/:url' do
-  #haml "%h1 #{params[:url]}"
-  url = Db.unshort params[:url]
-  redirect "https://#{url}"
-end
+App.run! if __FILE__ == $0
